ad2aadfe1e6cfd3e49b9858a2c96953fd80b7c6d
Mamet Ucup <aliminthekings@gmail.com>
2025-06-15 20:00:31 +0700

    Update nav-script.js

--- assets/nav-script.js
+++ assets/nav-script.js
@@ -14,16 +14,3 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
-// Tambahkan di nav-script.js
-document.addEventListener("DOMContentLoaded", () => {
-  const container = document.querySelector(".meteor-container");
-
-  for (let i = 0; i < 20; i++) {
-    const meteor = document.createElement("div");
-    meteor.classList.add("meteor");
-    meteor.style.left = Math.random() * 100 + "vw";
-    meteor.style.animationDelay = Math.random() * 5 + "s";
-    meteor.style.animationDuration = 1.5 + Math.random() * 1 + "s";
-    container.appendChild(meteor);
-  }
-});
