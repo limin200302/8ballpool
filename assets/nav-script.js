5fa978f422af9b7c295b24f0e510ed18eb28bb9b
Mamet Ucup <aliminthekings@gmail.com>
2025-06-15 18:45:58 +0700

    Create nav-script.js

---
+++ assets/nav-script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.querySelector(".hamburger");
+  const sideNav = document.querySelector(".side-nav");
+
+  hamburger.addEventListener("click", () => {
+    sideNav.classList.toggle("open");
+  });
+});
