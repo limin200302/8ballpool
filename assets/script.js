2d48d6d1262844daa4d91fd17dc0f16f8608ca59
Mamet Ucup <aliminthekings@gmail.com>
2025-06-15 12:36:05 +0700

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -249,14 +249,3 @@ document.getElementById("modeToggle").addEventListener("click", () => {
 
 generatePriceList("priceListCash", vipDataCash, "assets/img/dollar.png");
 generatePriceList("boxLegend", vipDataBox, "assets/img/box_legends.png");
-
-document.querySelectorAll("#poolPass .item button").forEach(button => {
-  button.addEventListener("click", () => {
-    const item = button.closest(".item");
-    const span = item.querySelector("span");
-    const itemName = "Pool Pass - " + span.textContent.trim();
-    const price = parseInt(span.textContent.replace(/\D/g, ""));
-
-    addToCart(itemName, price, button);
-  });
-});
